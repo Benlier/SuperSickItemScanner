5f52c44eb6a2a4a110a59b61c57d0cf31c196d27
Benlier <ben.lierman@gmail.com>
2020-12-17 19:36:47 +0100

    Catching UnicodeDecodeError

--- player_inv.py
+++ player_inv.py
@@ -22,7 +22,10 @@ def list_players_items(player_files: [str]):
     scanned_players = 0
 
     for player_file in player_files:
-        player = nbt.nbt.NBTFile(player_file)
+        try:
+            player = nbt.nbt.NBTFile(player_file)
+        except UnicodeDecodeError:
+            print(player_file)
         players_items.update(list_player_items(player))
         scanned_players += 1
         print(str(scanned_players) + "/" + player_count)
